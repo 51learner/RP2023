53cadc75ce08dbaa2fdfae7a1a647094ed4b3e69
RMER <107309844+51learner@users.noreply.github.com>
2022-09-27 15:28:04 +0800

    第一次上传

---
+++ rmer2023/rmer2023/MDK-ARM/RTE/_rp_template_hal_v1/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'rmer2023' 
+ * Target:  'rp_template_hal_v1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
